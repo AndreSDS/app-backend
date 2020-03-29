c429f7d86a4188e0822f7a0d9e80cc5e1794a1a3
andresds <andre_smiths@outlook.com>
2020-03-29 00:13:41 -0300

    alterando validação ra rota de login

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.post('/session', celebrate({
-    [Segments.PARAMS] : Joi.object().keys({
+    [Segments.BODY] : Joi.object().keys({
         id: Joi.string().required(),
     }),
 }), SessionController.create);
@@ -20,7 +20,7 @@ routes.post('/ongs', celebrate({
     [Segments.BODY] : Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
-        whatsapp: Joi.string().required().min(9).max(11),
+        whatsapp: Joi.string().required().min(8).max(11),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2),
     })
