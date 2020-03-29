1627e558300d54b841e2449ac23ece8b2122bd10
andresds <andre_smiths@outlook.com>
2020-03-28 22:38:05 -0300

    tentando deploy no heroku

---
+++ src/app.js
@@ -0,0 +1,19 @@
+/*
+tipos de parâmetros
+    // request.query - parametros nomeados 
+    // request.params - parametros para identificar recursos
+    // request.body - utilizado para criar ou alterar recursos
+*/
+const routes = require('./routes');
+const express = require('express');
+const cors = require('cors');
+const { errors } = require('celebrate');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
---
+++ src/controllers/OngController.js
@@ -0,0 +1,27 @@
+const generateUnicId = require('../utils/generateUnicId');
+const connection = require('../database/connection');
+const crypto = require('crypto');
+
+module.exports = {
+    async create (req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+        
+        const id = generateUnicId();
+    
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        });
+
+        return res.json({ id });
+    },
+
+    async index (req, res) {
+        const ongs = await connection('ongs').select("*");
+        return res.json(ongs);
+    }
+}
